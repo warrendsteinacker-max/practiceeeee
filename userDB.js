3ef1cff9b0602a4492e1fee88e7369a12e93c1cd
Warren <warrendsteinacker@gmail.com>
2025-12-18 08:19:39 -0500

    think I created proper data base schema and model

--- userDB.js
+++ userDB.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
 const userschem = new mongoose.Schema({
+    person: {
+        id: Number,
+        name: String,
+    }
 
-    
 })
+
+const User = mongoose.model('User', 'userschem')
+
+module.exports User
