72f8537d246113b1c07589f1f6b43d392a64f8a9
Warren <warrendsteinacker@gmail.com>
2025-12-19 10:05:03 -0500

    think I created proper admin cheak

--- index.js
+++ index.js
@@ -46,7 +46,28 @@ const TK = (req, res, next) => {
     }
 }
 
+const Acheack = (req, res, next) => {
 
+  const {token} = req.cookies
+
+  if(!token){
+    return res.status(401).json({error: D})
+  }
+
+  try{
+    const dec = jwt.verify(token, process.env.JWT)
+    if(dec.role === "admin"){
+      req.user = dec
+      next()
+    }
+    else{
+      return res.status(403).json({error: D})
+    } 
+  }
+  catch(error.message)
+    console.error(error.message)
+    return res.status(401).json({error: D})
+}
 
 
 const Acheack = (req, res, next) => {
