a814d78f302aa9fd484b19a91f3d7fa1c1be4162
Warren <warrendsteinacker@gmail.com>
2025-12-19 11:50:45 -0500

    made my pre schema for makeing salt rounds and hashing the pas

--- userDB.js
+++ userDB.js
@@ -10,7 +10,19 @@ const userschem = new mongoose.Schema({
     }
 });
 
-pre.userschem for hashing the password
+userschem.pre('save', async function(next) {
+    // 'this' refers to the user document
+    // We only hash if the password is new or being changed
+    if (!this.isModified('person.password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.person.password = await bcrypt.hash(this.person.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 
 
