18debce3d79bd251561df1427b6d53dc1d9b97bf
Warren <warrendsteinacker@gmail.com>
2025-12-20 11:26:14 -0500

    formulating more notes

---
+++ routes for other project.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom"
+
+
+
+
+export const App = () => {
+    
+    
+    
+    
+    return(
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route></Route>    //////admin home page were he can see all the users with buttons for delet and Link for each user to edit page were he can edit there profile and a link for each user were he can view there posts and delete them
+        <Route></Route>     ////// admin edit page for users and there roles
+        <Route></Route>     ////// admin creat new User page
+        <Route></Route>   ///////// user page to update edit there post
+        <Route></Route>     ////////
+        <Route></Route>     ////////// user home page with creat new post link that brings them to post page page 
+    </Routes>
+    </BrowserRouter>
+    </>
+    )
+}
