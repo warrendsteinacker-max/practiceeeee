8756862e9e157d25b5951b1e840c9ad51ddd6370
Warren <warrendsteinacker@gmail.com>
2025-12-20 09:19:15 -0500

    formulating idea for how mush state I will need

--- practice.jsx
+++ practice.jsx
@@ -1,1 +1,23 @@
-import React, {} from 'react'
+import React,{ useEffect, createContext, useState } from 'react';
+
+
+
+
+export const FeatureContext = createContext(null);
+
+export const DataProvider = async ({children}) => {
+    ///usestate for fetching posts
+    const [] = useState
+    ///usestate for fetching users
+    const [] = useState
+    ///usestate for api for posts
+    const [] = useState
+
+    ///usestate for api for authentication
+
+    return(<FeatureContext.Provider value={{}}>{children}</FeatureContext.Provider>)
+
+}
+
+
+
